71203f3252dc1176999f7d210f8ec7941913771d
Eric Winbigler <uzof99@gmail.com>
2019-06-25 13:15:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -53,7 +53,7 @@ def using_insert(array, element)
     flat_array= instruments.flatten
   end
   
-  def using_delete()
+  def using_delete(array,)
     instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
      no_offense_steven =instructors.delete("Steven")
   end
